aef96dec2b3c87eeb826d82f7d74014e99bb7952
Santosh Shrestha <sthasantosh070@gmail.com>
2024-12-26 15:11:50 +0545

    Changes done

--- app/src/main/java/com/example/footballkitstore/DashboardActivity2.kt
+++ app/src/main/java/com/example/footballkitstore/DashboardActivity2.kt
@@ -1,16 +1,26 @@
 package com.example.footballkitstore
 
+
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Dashboard : AppCompatActivity() {
+class DashboardActivity2 : AppCompatActivity() {
+    lateinit var imageView: ImageView
+    lateinit var button: Button
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_dashboard)
+        setContentView(R.layout.activity_dashboard2)
+        imageView = findViewById(R.id.logo)
+        button = findViewById(R.id.startshopping)
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
